56495946eae049389711cd847c2949da269a8e31
Emre Kardaşlar <32782530+emrekardaslar@users.noreply.github.com>
2020-03-10 09:18:02 +0300

    Update bot.py

--- translateBot/bot.py
+++ translateBot/bot.py
@@ -1,4 +1,4 @@
-import discord, ffmpeg, requests
+import discord, ffmpeg, requests, sys
 from gtts import gTTS
 from discord.ext import commands
 from city import City
@@ -31,16 +31,30 @@ async def text2Speech(language,text,ctx,channel):
         ctx.voice_client.play(discord.FFmpegPCMAudio('output.mp3'))
     
 @bot.command(name='t')
-async def t2s(ctx,arg1,arg2):
+async def t2s(ctx,*argv):
     channel = ctx.author.voice.channel
-    await text2Speech(arg1,arg2,ctx,channel)
+    text = ""
+    for i in range(1,len(argv)):
+        text += argv[i]
+    language = argv[0]
+    await text2Speech(language,text,ctx,channel)
     #await ctx.send("Lang is {} and the sentence is:  {} ".format(arg1,arg2))
 
 @bot.command(name='w')
 async def weather(ctx,city):
     city = City(city)
     strCity = city.getData()
-    print(strCity)
     await ctx.send(strCity)
 
-bot.run(token)
+@bot.command(name='fcast')
+async def weather(ctx,language,name):
+    channel = ctx.author.voice.channel
+    city = City(name)
+    data = city.getData()
+    if language == "en":
+        await text2Speech(language,data,ctx,channel)
+    elif language == "tr":
+        data = city.getName() + " şehrinde hava " + str(city.convert(city.getTemp())) + "derece."
+        await text2Speech(language,data,ctx,channel)
+
+bot.run("NjgyMjk3MDEzMTczNDg1NTgx.Xla8pA.ZEVEqEPM6qSPr8weYy8HG7LgAdo")
